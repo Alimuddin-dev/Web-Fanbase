7472506c8c810527192813cf473e7624236ddb03
Alimuddin-dev <almudalimuddin@gmail.com>
2023-04-19 22:14:11 +0800

    ubah menjadi website fanbase hhh

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#image-slider").mount();
+});
+
+// toogle class active
+const burger = document.querySelector(".burger");
+const sidebar = document.querySelector(".sidebar");
+const bgSidebar = document.querySelector(".bg-sidebar");
+
+burger.addEventListener("click", function () {
+  this.classList.toggle("change");
+  sidebar.classList.toggle("change");
+  bgSidebar.classList.toggle("change");
+});
+
+bgSidebar.addEventListener("click", function () {
+  this.classList.remove("change");
+  sidebar.classList.remove("change");
+  burger.classList.remove("change");
+});
